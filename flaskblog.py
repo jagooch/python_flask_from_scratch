7815b957236e7807414dec445748fe434811c766
John Gooch <john@goochdom.com>
2020-03-03 22:27:08 -0700

    Added registration form with validation and feedback.

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
 import sqlite3
 from pprint import pprint
+from forms import RegistrationForm, LoginForm
 
 
 with sqlite3.connect("posts.db") as conn:
@@ -13,6 +17,8 @@ with sqlite3.connect("posts.db") as conn:
 pprint(posts)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '894f16ed082df645715f80256549fef5'
+
 
 @app.route("/")
 @app.route("/home")
@@ -23,6 +29,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='about')
+
+@app.route("/register", methods=[ 'GET', 'POST'  ])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect( url_for('home') ) 
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
